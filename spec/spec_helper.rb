94eb2b7487a4d95ff3d8cc1a3a530860c1034d48
Brooke Kuhlmann <brooke@alchemists.io>
2015-11-02 14:47:55 -0700

    Refactored RSpec Pry support as an extension.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,16 +6,7 @@ if ENV["CODECLIMATE_REPO_TOKEN"]
 end
 
 require "sublime_text_kit"
-require "pry"
-require "pry-byebug"
-require "pry-state"
-require "pry-stack_explorer"
-require "pry-remote"
-require "pry-rescue"
 
+Dir[File.join(File.dirname(__FILE__), "support/extensions/**/*.rb")].each { |file| require file }
 Dir[File.join(File.dirname(__FILE__), "support/kit/**/*.rb")].each { |file| require file }
 Dir[File.join(File.dirname(__FILE__), "support/shared_examples/**/*.rb")].each { |file| require file }
-
-# Uncomment to add a custom configuration. For the default configuration, see the "support/kit" folder.
-# RSpec.configure do |config|
-# end
