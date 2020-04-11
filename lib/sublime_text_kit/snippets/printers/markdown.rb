b8a0938e299164c0ee0746c2cd040efad2e3a649
Brooke Kuhlmann <brooke@alchemists.io>
2020-04-11 13:10:10 -0600

    Refactored Markdown snippet printer to use collector
    Simplifies the implementation by removing duplicate code now found in
    the `Collector`.

--- lib/sublime_text_kit/snippets/printers/markdown.rb
+++ lib/sublime_text_kit/snippets/printers/markdown.rb
@@ -1,41 +1,22 @@
 # frozen_string_literal: true
 
-require "rexml/document"
-
 module SublimeTextKit
   module Snippets
     module Printers
       class Markdown
-        DEFAULT_USER_PATH = "Library/Application Support/Sublime Text 3/Packages/User"
-
-        def initialize user_path = DEFAULT_USER_PATH, environment: ENV
-          @user_path = user_path
-          @environment = environment
-        end
-
-        def home_path
-          Pathname environment.fetch("HOME")
-        end
-
-        def root_path
-          home_path.join user_path
+        def initialize collector: Collector.new
+          @collector = collector
         end
 
         def call
-          snippets.sort_by(&:description).each do |snippet|
+          collector.call.each do |snippet|
             puts "- #{snippet.description} - `#{snippet.trigger}`\n"
           end
         end
 
         private
 
-        attr_reader :user_path, :environment
-
-        def snippets
-          root_path.glob("*.sublime-snippet").map do |path|
-            Snippet.new REXML::Document.new(File.open(path))
-          end
-        end
+        attr_reader :collector
       end
     end
   end
--- spec/lib/sublime_text_kit/snippets/printers/markdown_spec.rb
+++ spec/lib/sublime_text_kit/snippets/printers/markdown_spec.rb
@@ -3,8 +3,9 @@
 require "spec_helper"
 
 RSpec.describe SublimeTextKit::Snippets::Printers::Markdown, :temp_dir do
-  subject(:printer) { described_class.new environment: environment }
+  subject(:printer) { described_class.new collector: collector }
 
+  let(:collector) { SublimeTextKit::Snippets::Collector.new environment: environment }
   let(:environment) { {"HOME" => temp_dir} }
   let(:support_path) { Bundler.root.join "spec/support/snippets" }
   let(:snippets_path) { temp_dir.join "Library/Application Support/Sublime Text 3/Packages/User" }
@@ -12,7 +13,7 @@ RSpec.describe SublimeTextKit::Snippets::Printers::Markdown, :temp_dir do
   describe "#call" do
     context "with snippets" do
       before do
-        FileUtils.mkdir_p snippets_path
+        snippets_path.mkpath
         FileUtils.cp_r "#{support_path}/.", snippets_path
       end
 
