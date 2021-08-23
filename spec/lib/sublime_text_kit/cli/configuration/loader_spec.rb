175c020396f0567ba16cd07fcecf6cb052f6d7b9
Brooke Kuhlmann <brooke@alchemists.io>
2021-08-23 16:02:39 -0600

    Added CLI configuration loader
    Necessary to provide the primary CLI configuration for the gem which
    loads the default configuration and encapsulates the full Runcom
    interface.

---
+++ spec/lib/sublime_text_kit/cli/configuration/loader_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe SublimeTextKit::CLI::Configuration::Loader do
+  subject(:loader) { described_class.with_defaults }
+
+  let :content do
+    SublimeTextKit::CLI::Configuration::Content[
+      action_config: nil,
+      action_help: nil,
+      action_metadata: nil,
+      action_session: nil,
+      action_snippets: nil,
+      action_update: nil,
+      action_version: nil,
+      project_roots: nil,
+      metadata_dir: nil,
+      snippets_format: :markdown,
+      session_path: Pathname(
+        %(#{ENV["HOME"]}/Library/Application Support/Sublime Text/Local/Session.sublime_session)
+      ),
+      user_dir: Pathname(%(#{ENV["HOME"]}/Library/Application Support/Sublime Text/Packages/User))
+    ]
+  end
+
+  describe ".call" do
+    it "answers default configuration" do
+      expect(described_class.call).to be_a(SublimeTextKit::CLI::Configuration::Content)
+    end
+  end
+
+  describe ".with_defaults" do
+    it "answers default configuration" do
+      expect(described_class.with_defaults.call).to eq(content)
+    end
+  end
+
+  describe "#call" do
+    it "answers default configuration" do
+      expect(loader.call).to eq(content)
+    end
+  end
+end
