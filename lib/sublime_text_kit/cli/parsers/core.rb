46a886aa87b844b8c881350a6ca921520e4cac7c
Brooke Kuhlmann <brooke@alchemists.io>
2021-08-23 16:22:08 -0600

    Added CLI core parser
    Necessary to provide the primary command line interface usage options.

---
+++ lib/sublime_text_kit/cli/parsers/core.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+require "versionaire/extensions/option_parser"
+
+module SublimeTextKit
+  module CLI
+    # Handles parsing of Command Line Interface (CLI) primary options.
+    module Parsers
+      # Handles parsing of Command Line Interface (CLI) core options.
+      class Core
+        def self.call(...) = new(...).call
+
+        def initialize client: CLIENT, container: Container
+          @client = client
+          @container = container
+        end
+
+        def call arguments = []
+          client.banner = "#{Identity::LABEL} - #{Identity::SUMMARY}"
+          client.separator "\nUSAGE:\n"
+          collate
+          arguments.empty? ? arguments : client.parse!(arguments)
+        end
+
+        private
+
+        attr_reader :client, :container
+
+        def collate = private_methods.sort.grep(/add_/).each { |method| __send__ method }
+
+        def add_config
+          client.on(
+            "-c",
+            "--config ACTION",
+            %i[edit view],
+            "Manage gem configuration. Actions: edit or view."
+          ) do |action|
+            configuration.action_config = action
+          end
+        end
+
+        def add_metadata
+          client.on(
+            "-m",
+            "--metadata ACTION",
+            %i[create delete recreate],
+            "Manage metadata. Actions: create, delete, or recreate."
+          ) do |action|
+            configuration.action_metadata = action
+          end
+        end
+
+        def add_session
+          client.on "-S", "--session", "Rebuild session." do
+            configuration.action_session = true
+          end
+        end
+
+        def add_snippets
+          client.on(
+            "-s",
+            "--snippets [FORMAT]",
+            %i[markdown ascii_doc],
+            "View snippets. Default: #{configuration.snippets_format}. " \
+            "Formats: markdown or ascii_doc."
+          ) do |kind|
+            configuration.action_snippets = true
+            configuration.snippets_format = kind if kind
+          end
+        end
+
+        def add_update
+          client.on "-u", "--update", "Update metadata and session based on current settings." do
+            configuration.action_update = true
+          end
+        end
+
+        def add_version
+          client.on "-v", "--version", "Show gem version." do
+            configuration.action_version = Identity::VERSION_LABEL
+          end
+        end
+
+        def add_help
+          client.on "-h", "--help", "Show this message." do
+            configuration.action_help = true
+          end
+        end
+
+        private_methods
+
+        def configuration = container[__method__]
+      end
+    end
+  end
+end
---
+++ spec/lib/sublime_text_kit/cli/parsers/core_spec.rb
@@ -0,0 +1,155 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe SublimeTextKit::CLI::Parsers::Core do
+  using Versionaire::Cast
+
+  subject(:parser) { described_class.new }
+
+  include_context "with application container"
+
+  it_behaves_like "a parser"
+
+  describe "#call" do
+    it "answers config edit (short)" do
+      parser.call %w[-c edit]
+      expect(configuration.action_config).to eq(:edit)
+    end
+
+    it "answers config edit (long)" do
+      parser.call %w[--config edit]
+      expect(configuration.action_config).to eq(:edit)
+    end
+
+    it "answers config view (short)" do
+      parser.call %w[-c view]
+      expect(configuration.action_config).to eq(:view)
+    end
+
+    it "answers config view (long)" do
+      parser.call %w[--config view]
+      expect(configuration.action_config).to eq(:view)
+    end
+
+    it "fails with missing config action" do
+      expectation = proc { parser.call %w[--config] }
+      expect(&expectation).to raise_error(OptionParser::MissingArgument, /--config/)
+    end
+
+    it "fails with invalid config action" do
+      expectation = proc { parser.call %w[--config bogus] }
+      expect(&expectation).to raise_error(OptionParser::InvalidArgument, /bogus/)
+    end
+
+    it "answers metadata create (short)" do
+      parser.call %w[-m create]
+      expect(configuration.action_metadata).to eq(:create)
+    end
+
+    it "answers metadata create (long)" do
+      parser.call %w[--metadata create]
+      expect(configuration.action_metadata).to eq(:create)
+    end
+
+    it "answers metadata delete (short)" do
+      parser.call %w[-m delete]
+      expect(configuration.action_metadata).to eq(:delete)
+    end
+
+    it "answers metadata delete (long)" do
+      parser.call %w[--metadata delete]
+      expect(configuration.action_metadata).to eq(:delete)
+    end
+
+    it "answers metadata recreate (short)" do
+      parser.call %w[-m recreate]
+      expect(configuration.action_metadata).to eq(:recreate)
+    end
+
+    it "answers metadata recreate (long)" do
+      parser.call %w[--metadata recreate]
+      expect(configuration.action_metadata).to eq(:recreate)
+    end
+
+    it "fails with missing metadata action" do
+      expectation = proc { parser.call %w[--metadata] }
+      expect(&expectation).to raise_error(OptionParser::MissingArgument, /--metadata/)
+    end
+
+    it "fails with invalid metadata action" do
+      expectation = proc { parser.call %w[--metadata bogus] }
+      expect(&expectation).to raise_error(OptionParser::InvalidArgument, /bogus/)
+    end
+
+    it "enables snippets (short)" do
+      parser.call %w[-s]
+      expect(configuration.action_snippets).to eq(true)
+    end
+
+    it "enables snippets (long)" do
+      parser.call %w[--snippets]
+      expect(configuration.action_snippets).to eq(true)
+    end
+
+    it "answers snippets ASCII Doc format" do
+      parser.call %w[--snippets ascii_doc]
+      expect(configuration.snippets_format).to eq(:ascii_doc)
+    end
+
+    it "answers snippets Markdown format" do
+      parser.call %w[--snippets markdown]
+      expect(configuration.snippets_format).to eq(:markdown)
+    end
+
+    it "answers snippets default format when no format is given" do
+      parser.call %w[--snippets]
+      expect(configuration.snippets_format).to eq(:markdown)
+    end
+
+    it "fails with invalid snippets action" do
+      expectation = proc { parser.call %w[--snippets bogus] }
+      expect(&expectation).to raise_error(OptionParser::InvalidArgument, /bogus/)
+    end
+
+    it "answers session rebuild (short)" do
+      parser.call %w[-S]
+      expect(configuration.action_session).to eq(true)
+    end
+
+    it "answers session rebuild (long)" do
+      parser.call %w[--session]
+      expect(configuration.action_session).to eq(true)
+    end
+
+    it "answers update (short)" do
+      parser.call %w[-u]
+      expect(configuration.action_update).to eq(true)
+    end
+
+    it "answers update (long)" do
+      parser.call %w[--update]
+      expect(configuration.action_update).to eq(true)
+    end
+
+    it "answers version (short)" do
+      parser.call %w[-v]
+      expect(configuration.action_version).to match(/Sublime Text Kit\s\d+\.\d+\.\d+/)
+    end
+
+    it "answers version (long)" do
+      parser.call %w[--version]
+      expect(configuration.action_version).to match(/Sublime Text Kit\s\d+\.\d+\.\d+/)
+    end
+
+    it "enables help (short)" do
+      parser.call %w[-h]
+      expect(configuration.action_help).to eq(true)
+    end
+
+    it "enables help (long)" do
+      parser.call %w[--help]
+      expect(configuration.action_help).to eq(true)
+    end
+  end
+end
