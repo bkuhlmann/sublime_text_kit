2216fcdea9876c1c80ae86ce7bc1c8732077fbce
Brooke Kuhlmann <brooke@alchemists.io>
2021-08-24 16:45:23 -0600

    Added application container
    Necessary to register core components for use throughout the entire
    gem.

---
+++ spec/support/shared_contexts/application_container.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "dry/container/stub"
+
+RSpec.shared_context "with application container" do
+  using Refinements::Structs
+
+  include_context "with temporary directory"
+
+  let(:container) { SublimeTextKit::Container }
+
+  let :configuration do
+    SublimeTextKit::CLI::Configuration::Loader.with_defaults.call.merge(
+      project_roots: [Bundler.root.join("spec/support/projects")],
+      metadata_dir: temp_dir,
+      session_path: temp_dir.join("Session.sublime_session"),
+      user_dir: Bundler.root.join("spec/support/snippets")
+    )
+  end
+
+  let(:kernel) { class_spy Kernel }
+
+  before do
+    container.enable_stubs!
+    container.stub :configuration, configuration
+    container.stub :kernel, kernel
+  end
+
+  after { container.unstub :kernel }
+end
