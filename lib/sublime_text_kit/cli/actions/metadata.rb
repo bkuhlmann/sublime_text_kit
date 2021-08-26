90e41acd9605dd33fed75a418c92bf21c4d49898
Brooke Kuhlmann <brooke@alchemists.io>
2021-08-26 14:28:17 -0600

    Added CLI metadata action
    Necessary to handle the creation, deletion, and recreation of metadata.

---
+++ lib/sublime_text_kit/cli/actions/metadata.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require "refinements/pathnames"
+
+module SublimeTextKit
+  module CLI
+    module Actions
+      # Handles metadata action.
+      class Metadata
+        using Refinements::Pathnames
+
+        def initialize handler: SublimeTextKit::Metadata::Handler, container: Container
+          @handler = handler
+          @container = container
+        end
+
+        def call kind
+          case kind
+            when :create then create
+            when :delete then delete
+            when :recreate then recreate
+            else logger.error "Unknown metadata action: #{kind}."
+          end
+        end
+
+        private
+
+        attr_reader :handler, :container
+
+        def create
+          logger.info "Creating metadata in #{metadata_dir}..."
+          process_projects __method__
+          logger.info "Metadata created."
+        end
+
+        def delete
+          logger.info "Deleting metadata in #{metadata_dir}..."
+          process_projects __method__
+          logger.info "Metadata deleted."
+        end
+
+        def recreate
+          logger.info "Recreating metadata in #{metadata_dir}..."
+          process_projects __method__
+          logger.info "Metadata recreated."
+        end
+
+        def process_projects method
+          configuration.project_dirs.each do |directory|
+            handler.with_project(directory, metadata_dir).public_send method
+            handler.with_workspace(directory, metadata_dir).public_send method
+          end
+        end
+
+        def metadata_dir = Pathname(configuration.metadata_dir).expand_path
+
+        def configuration = container[__method__]
+
+        def logger = container[__method__]
+      end
+    end
+  end
+end
---
+++ spec/lib/sublime_text_kit/cli/actions/metadata_spec.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe SublimeTextKit::CLI::Actions::Metadata do
+  using Refinements::Pathnames
+
+  subject(:action) { described_class.new }
+
+  include_context "with application container"
+
+  describe "#call" do
+    it "creates metadata" do
+      action.call :create
+      file_names = temp_dir.files.map { |path| path.basename.to_s }
+
+      expect(file_names).to contain_exactly(
+        "black.sublime-project",
+        "black.sublime-workspace",
+        "red.sublime-project",
+        "red.sublime-workspace",
+        "white.sublime-project",
+        "white.sublime-workspace"
+      )
+    end
+
+    it "logs metadata being created" do
+      expectation = proc { action.call :create }
+      message = "Creating metadata in #{temp_dir}...\nMetadata created.\n"
+
+      expect(&expectation).to output(message).to_stdout
+    end
+
+    it "deletes metadata" do
+      Bundler.root.join("spec/support/metadata").files.each { |path| path.copy temp_dir }
+      action.call :delete
+
+      expect(temp_dir.files.empty?).to eq(true)
+    end
+
+    it "logs metadata being deleted" do
+      expectation = proc { action.call :delete }
+      message = "Deleting metadata in #{temp_dir}...\nMetadata deleted.\n"
+
+      expect(&expectation).to output(message).to_stdout
+    end
+
+    it "recreates metadata" do
+      action.call :recreate
+      file_names = temp_dir.files.map { |path| path.basename.to_s }
+
+      expect(file_names).to contain_exactly(
+        "black.sublime-project",
+        "black.sublime-workspace",
+        "red.sublime-project",
+        "red.sublime-workspace",
+        "white.sublime-project",
+        "white.sublime-workspace"
+      )
+    end
+
+    it "logs metadata being recreated" do
+      expectation = proc { action.call :recreate }
+      message = "Recreating metadata in #{temp_dir}...\nMetadata recreated.\n"
+
+      expect(&expectation).to output(message).to_stdout
+    end
+
+    it "fails when given invalid action" do
+      expectation = proc { action.call :bogus }
+      expect(&expectation).to output("Unknown metadata action: bogus.\n").to_stdout
+    end
+  end
+end
