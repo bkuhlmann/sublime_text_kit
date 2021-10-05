4d181ee188f6b7905252454cc76f31d8f54684bb
Brooke Kuhlmann <brooke@alchemists.io>
2021-10-04 20:03:09 -0600

    Added CLI update action
    Necessary to process the update actions from the CLI which wraps the
    creation of metadata along with the corresponding updating of session
    details.

---
+++ lib/sublime_text_kit/cli/actions/update.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module SublimeTextKit
+  module CLI
+    module Actions
+      # Handles update action.
+      class Update
+        def initialize metadata: SublimeTextKit::Metadata::Handler,
+                       session: Sessions::Rebuilder.new,
+                       container: Container
+          @metadata = metadata
+          @session = session
+          @container = container
+        end
+
+        def call
+          logger.info "Updating metadata and session..."
+          create_metadata
+          session.call
+          logger.info "Metadata and session updated."
+        end
+
+        private
+
+        attr_reader :metadata, :session, :container
+
+        def create_metadata
+          configuration.project_dirs.each do |directory|
+            metadata.with_project(directory, metadata_dir).create
+            metadata.with_workspace(directory, metadata_dir).create
+          end
+        end
+
+        def metadata_dir = Pathname(configuration.metadata_dir).expand_path
+
+        def configuration = container[__method__]
+
+        def logger = container[__method__]
+      end
+    end
+  end
+end
---
+++ spec/lib/sublime_text_kit/cli/actions/update_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe SublimeTextKit::CLI::Actions::Update do
+  using Refinements::Pathnames
+
+  subject(:action) { described_class.new session: session }
+
+  include_context "with application container"
+
+  let(:session) { instance_spy SublimeTextKit::Sessions::Rebuilder }
+
+  describe "#call" do
+    it "creates metadata" do
+      action.call
+      file_names = temp_dir.files.map { |path| path.basename.to_s }
+
+      expect(file_names).to contain_exactly(
+        "black.sublime-project",
+        "black.sublime-workspace",
+        "red.sublime-project",
+        "red.sublime-workspace",
+        "white.sublime-project",
+        "white.sublime-workspace"
+      )
+    end
+
+    it "rebuilds session" do
+      action.call
+      expect(session).to have_received(:call)
+    end
+
+    it "logs status" do
+      expectation = proc { action.call }
+      message = "Updating metadata and session...\nMetadata and session updated.\n"
+
+      expect(&expectation).to output(message).to_stdout
+    end
+  end
+end
