75ec91d654bbc04a5318bfddbe784662692253c4
Brooke Kuhlmann <brooke@alchemists.io>
2023-04-21 14:20:21 -0600

    Added CLI metadata recreate action
    Necessary to split this out into it's own object in order to reduce complexity with the original metadata action.

---
+++ lib/sublime_text_kit/cli/actions/metadata/recreate.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "refinements/pathnames"
+require "sod"
+
+module SublimeTextKit
+  module CLI
+    module Actions
+      module Metadata
+        # Recreates project metadata.
+        class Recreate < Sod::Action
+          include Import[:configuration, :logger]
+
+          using Refinements::Pathnames
+
+          description "Recreate metadata."
+
+          on %w[-r --recreate]
+
+          def initialize(handler: SublimeTextKit::Metadata::Handler, **)
+            super(**)
+            @handler = handler
+          end
+
+          def call(*)
+            logger.info "Recreating metadata in #{metadata_dir}..."
+            process_projects
+            logger.info "Metadata recreated."
+          end
+
+          private
+
+          attr_reader :handler
+
+          def process_projects
+            configuration.project_dirs.each do |directory|
+              handler.with_project(directory, metadata_dir).recreate
+              handler.with_workspace(directory, metadata_dir).recreate
+            end
+          end
+
+          def metadata_dir = Pathname(configuration.metadata_dir).expand_path
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/sublime_text_kit/cli/actions/metadata/recreate_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe SublimeTextKit::CLI::Actions::Metadata::Recreate do
+  using Refinements::Pathnames
+
+  subject(:action) { described_class.new }
+
+  include_context "with application dependencies"
+
+  describe "#call" do
+    it "recreates metadata" do
+      action.call :recreate
+      file_names = temp_dir.files.map { |path| path.basename.to_s }
+
+      expect(file_names).to contain_exactly(
+        "black.sublime-project",
+        "black.sublime-workspace",
+        "red.sublime-project",
+        "red.sublime-workspace",
+        "white.sublime-project",
+        "white.sublime-workspace"
+      )
+    end
+
+    it "logs metadata being recreated" do
+      action.call :recreate
+
+      expect(logger.reread).to match(
+        /🟢.+Recreating metadata in #{temp_dir}....+\n🟢.+Metadata recreated./
+      )
+    end
+  end
+end
