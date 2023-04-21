3b2dd4134e43976ee37df9f0db842022354dad0b
Brooke Kuhlmann <brooke@alchemists.io>
2023-04-21 14:17:47 -0600

    Added CLI metadata delete action
    Necessary to split this out into it's own object in order to reduce complexity with the original metadata action.

---
+++ lib/sublime_text_kit/cli/actions/metadata/delete.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "refinements/pathnames"
+require "sod"
+
+module SublimeTextKit
+  module CLI
+    module Actions
+      module Metadata
+        # Deletes project metadata.
+        class Delete < Sod::Action
+          include Import[:configuration, :logger]
+
+          using Refinements::Pathnames
+
+          description "Delete metadata."
+
+          on %w[-d --delete]
+
+          def initialize(handler: SublimeTextKit::Metadata::Handler, **)
+            super(**)
+            @handler = handler
+          end
+
+          def call(*)
+            logger.info "Deleting metadata in #{metadata_dir}..."
+            process_projects
+            logger.info "Metadata deleted."
+          end
+
+          private
+
+          attr_reader :handler
+
+          def process_projects
+            configuration.project_dirs.each do |directory|
+              handler.with_project(directory, metadata_dir).delete
+              handler.with_workspace(directory, metadata_dir).delete
+            end
+          end
+
+          def metadata_dir = Pathname(configuration.metadata_dir).expand_path
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/sublime_text_kit/cli/actions/metadata/delete_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe SublimeTextKit::CLI::Actions::Metadata::Delete do
+  using Refinements::Pathnames
+
+  subject(:action) { described_class.new }
+
+  include_context "with application dependencies"
+
+  describe "#call" do
+    it "deletes metadata" do
+      Bundler.root
+             .join("spec/support/fixtures/fixtures/metadata")
+             .files
+             .each { |path| path.copy temp_dir }
+      action.call :delete
+
+      expect(temp_dir.files.empty?).to be(true)
+    end
+
+    it "logs metadata being deleted" do
+      action.call :delete
+
+      expect(logger.reread).to match(
+        /🟢.+Deleting metadata in #{temp_dir}....+\n🟢.+Metadata deleted./
+      )
+    end
+  end
+end
