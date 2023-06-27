79eb9d9b78f34818e498d9baf97e41e3a2b51c01
Brooke Kuhlmann <brooke@alchemists.io>
2023-06-27 11:06:20 -0600

    Removed configuration model freezing
    Redundant since the Etcher gem takes care of this for us.
    
    Release: minor
    Format: asciidoc

--- lib/sublime_text_kit/configuration/model.rb
+++ lib/sublime_text_kit/configuration/model.rb
@@ -5,19 +5,8 @@ require "refinements/pathnames"
 module SublimeTextKit
   module Configuration
     # Models the configuration.
-    Model = Struct.new(
-      :project_roots,
-      :metadata_dir,
-      :snippets_format,
-      :session_path,
-      :user_dir
-    ) do
+    Model = Struct.new :project_roots, :metadata_dir, :snippets_format, :session_path, :user_dir do
       using Refinements::Pathnames
-
-      def initialize(**)
-        super
-        freeze
-      end
 
       def project_dirs
         Array(project_roots).map { |path| Pathname(path).expand_path }
