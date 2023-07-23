80dbf5f0e31b7fd963b2611d613d1ed251bdfa2e
Brooke Kuhlmann <brooke@alchemists.io>
2023-07-23 14:28:38 -0600

    Added container memoization
    Necessary to improve CLI boot and test suite performance by reducing unnecessary reloading of XDG configuration and related objects.

--- lib/sublime_text_kit/container.rb
+++ lib/sublime_text_kit/container.rb
@@ -11,7 +11,7 @@ module SublimeTextKit
   module Container
     extend Dry::Container::Mixin
 
-    register :configuration do
+    register :configuration, memoize: true do
       self[:defaults].add_loader(Etcher::Loaders::Environment.new(%w[HOME]))
                      .add_loader(Etcher::Loaders::YAML.new(self[:xdg_config].active))
                      .add_transformer(Configuration::Transformers::SessionPath.new)
@@ -19,15 +19,18 @@ module SublimeTextKit
                      .then { |registry| Etcher.call registry }
     end
 
-    register :defaults do
+    register :defaults, memoize: true do
       Etcher::Registry.new(contract: Configuration::Contract, model: Configuration::Model)
                       .add_loader(Etcher::Loaders::YAML.new(self[:defaults_path]))
     end
 
-    register(:defaults_path) { Pathname(__dir__).join("configuration/defaults.yml") }
-    register(:xdg_config) { Runcom::Config.new "sublime_text_kit/configuration.yml" }
-    register(:specification) { Spek::Loader.call "#{__dir__}/../../sublime_text_kit.gemspec" }
-    register(:kernel) { Kernel }
-    register(:logger) { Cogger.new formatter: :emoji }
+    register :specification, memoize: true do
+      Spek::Loader.call "#{__dir__}/../../sublime_text_kit.gemspec"
+    end
+
+    register(:defaults_path, memoize: true) { Pathname(__dir__).join("configuration/defaults.yml") }
+    register(:xdg_config, memoize: true) { Runcom::Config.new "sublime_text_kit/configuration.yml" }
+    register(:logger, memoize: true) { Cogger.new formatter: :emoji }
+    register :kernel, Kernel
   end
 end
